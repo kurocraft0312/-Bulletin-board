4d56c4ca007e34c0402dd75993731e9122a6d273
kurocraft0312 <kurocraft0312@gmail.com>
2019-11-01 12:23:27 +0900

    ノーマライズ・CSSを設置＆htmlをphp化しました。

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Bulletin-board</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="hidden" name="MAX_FILE_SIZE" value="">
+        <input type="file" name="image">
+        <input type="submit" value="upload">
+    </form>
+</body>
+</html>
